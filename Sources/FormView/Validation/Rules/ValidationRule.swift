49eafc53348a2203fcf78a48846d53d311ab222b
Yan Boyko <yanboyko21@gmail.com>
2025-03-18 18:50:42 +0300

    refactor

--- Sources/FormView/Validation/Rules/ValidationRule.swift
+++ Sources/FormView/Validation/Rules/ValidationRule.swift
@@ -13,12 +13,12 @@ public class ValidationRule {
     public let isExternal: Bool
     public let conditions: [ValidationBehaviour]
     
-    private let checkClosure: (String) async -> (String, Bool)
+    private let checkClosure: (String) async -> (Bool, String)
     
     internal required init(
         conditions: [ValidationBehaviour],
         isExternal: Bool,
-        checkClosure: @escaping (String) async -> (String, Bool)
+        checkClosure: @escaping (String) async -> (Bool, String)
     ) {
         self.message = .empty
         self.checkClosure = checkClosure
@@ -28,114 +28,110 @@ public class ValidationRule {
     
     public func check(value: String) async -> Bool {
         let checkResult = await checkClosure(value)
-        let message = checkResult.0
+        let message = checkResult.1
         self.message = message
         
-        return checkResult.1
+        return checkResult.0
     }
 }
 
 extension ValidationRule {
     public static func custom(
         conditions: [ValidationBehaviour],
-        checkClosure: @escaping (String) async -> (String, Bool)
+        checkClosure: @escaping (String) async -> (Bool, String)
     ) -> Self {
         return Self(conditions: conditions, isExternal: false, checkClosure: checkClosure)
     }
     
-    public static func external(checkClosure: @escaping (String) async -> (String, Bool)) -> Self {
+    public static func external(checkClosure: @escaping (String) async -> (Bool, String)) -> Self {
         return Self(conditions: [.manual], isExternal: true, checkClosure: checkClosure)
     }
     
     public static func notEmpty(conditions: [ValidationBehaviour], message: String) -> Self {
         return Self(conditions: conditions, isExternal: false) {
-            return $0.isEmpty == false ? (message, true) : (message, false)
+            return ($0.isEmpty == false, message)
         }
     }
     
     public static func atLeastOneLowercaseLetter(conditions: [ValidationBehaviour], message: String) -> Self {
         return Self(conditions: conditions, isExternal: false) {
-            return $0.rangeOfCharacter(from: CharacterSet.lowercaseLetters) != nil ? (message, true) : (message, false)
+            return ($0.rangeOfCharacter(from: CharacterSet.lowercaseLetters) != nil, message)
         }
     }
     
     public static func atLeastOneUppercaseLetter(conditions: [ValidationBehaviour], message: String) -> Self {
         return Self(conditions: conditions, isExternal: false) {
-            $0.rangeOfCharacter(from: CharacterSet.uppercaseLetters) != nil ? (message, true) : (message, false)
+            return ($0.rangeOfCharacter(from: CharacterSet.uppercaseLetters) != nil, message)
         }
     }
     
     public static func atLeastOneDigit(conditions: [ValidationBehaviour], message: String) -> Self {
         return Self(conditions: conditions, isExternal: false) {
-            $0.rangeOfCharacter(from: CharacterSet.decimalDigits) != nil ? (message, true) : (message, false)
+            return ($0.rangeOfCharacter(from: CharacterSet.decimalDigits) != nil, message)
         }
     }
     
     public static func atLeastOneLetter(conditions: [ValidationBehaviour], message: String) -> Self {
         return Self(conditions: conditions, isExternal: false) {
-            $0.rangeOfCharacter(from: CharacterSet.letters) != nil ? (message, true) : (message, false)
+            return ($0.rangeOfCharacter(from: CharacterSet.letters) != nil, message)
         }
     }
     
     public static func digitsOnly(conditions: [ValidationBehaviour], message: String) -> Self {
         return Self(conditions: conditions, isExternal: false) {
-            CharacterSet.decimalDigits.isSuperset(of: CharacterSet(charactersIn: $0))
-                ? (message, true)
-                : (message, false)
+            return (CharacterSet.decimalDigits.isSuperset(of: CharacterSet(charactersIn: $0)), message)
         }
     }
     
     public static func lettersOnly(conditions: [ValidationBehaviour], message: String) -> Self {
         return Self(conditions: conditions, isExternal: false) {
-            CharacterSet.letters.isSuperset(of: CharacterSet(charactersIn: $0)) ? (message, true) : (message, false)
+            return (CharacterSet.letters.isSuperset(of: CharacterSet(charactersIn: $0)), message)
         }
     }
     
     public static func atLeastOneSpecialCharacter(conditions: [ValidationBehaviour], message: String) -> Self {
         return Self(conditions: conditions, isExternal: false) {
-            $0.range(of: ".*[^A-Za-zА-Яа-яё0-9 ].*", options: .regularExpression) != nil
-                ? (message, true)
-                : (message, false)
+            return ($0.range(of: ".*[^A-Za-zА-Яа-яё0-9 ].*", options: .regularExpression) != nil, message)
         }
     }
     
     public static func noSpecialCharacters(conditions: [ValidationBehaviour], message: String) -> Self {
         return Self(conditions: conditions, isExternal: false) {
-            $0.range(of: ".*[^A-Za-zА-Яа-яё0-9 ].*", options: .regularExpression) == nil
-                ? (message, true)
-                : (message, false)
+            return ($0.range(of: ".*[^A-Za-zА-Яа-яё0-9 ].*", options: .regularExpression) == nil, message)
         }
     }
     
     public static func email(conditions: [ValidationBehaviour], message: String) -> Self {
         return Self(conditions: conditions, isExternal: false) {
-            NSPredicate(format: "SELF MATCHES %@", "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}")
-                .evaluate(with: $0) ? (message, true) : (message, false)
+            return (
+                NSPredicate(format: "SELF MATCHES %@", "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}")
+                    .evaluate(with: $0),
+                message
+            )
         }
     }
     
     public static func notRecurringPincode(conditions: [ValidationBehaviour], message: String) -> Self {
         return Self(conditions: conditions, isExternal: false) {
-            $0.range(of: "([0-9])\\1\\1\\1", options: .regularExpression) == nil ? (message, true) : (message, false)
+            return ($0.range(of: "([0-9])\\1\\1\\1", options: .regularExpression) == nil, message)
         }
     }
     
     public static func minLength(conditions: [ValidationBehaviour], count: Int, message: String) -> Self {
         return Self(conditions: conditions, isExternal: false) {
-            $0.count >= count ? (message, true) : (message, false)
+            return ($0.count >= count, message)
         }
     }
     
     public static func maxLength(conditions: [ValidationBehaviour], count: Int, message: String) -> Self {
         return Self(conditions: conditions, isExternal: false) {
-            $0.count <= count ? (message, true) : (message, false)
+            return ($0.count <= count, message)
         }
     }
     
     public static func regex(conditions: [ValidationBehaviour], value: String, message: String) -> Self {
         return Self(conditions: conditions, isExternal: false) {
-            NSPredicate(format: "SELF MATCHES %@", value)
-                .evaluate(with: $0) ? (message, true) : (message, false)
+            return (NSPredicate(format: "SELF MATCHES %@", value).evaluate(with: $0), message)
         }
     }
 }
